2ee4043542bdece1728ff0f42d08b8e0a8f95514
Lucaexe <lucarizzi04.lr@gmail.com>
2024-08-16 22:02:15 +0200

    add vueform dependencies and config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,11 +5,15 @@ import './index.css'
 import mitt from 'mitt'
 import VueApexCharts from "vue3-apexcharts";
 
+import Vueform from '@vueform/vueform'
+import vueformConfig from '../vueform.config.js'
+
 const emitter = mitt()
 const app = createApp(App)
 
 app.use(router)
 app.use(VueApexCharts);
+app.use(Vueform, vueformConfig)
 
 app.config.globalProperties.emitter = emitter
 app.mount('#app')
