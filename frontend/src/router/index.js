2a072d70ef476fc8ffab06dd2d10660e9a1acfcd
DavideDadomo <davidedadomo@gmail.com>
2024-08-17 00:07:33 +0200

    prova

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import IndexView from '../views/indexView.vue'
 import DashboardView from '../views/dashboardView.vue'
+import AboutView from '../views/aboutView.vue'
+import FaqView from '../views/faqView.vue'
+import ContactView from '../views/contactView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,7 +18,23 @@ const router = createRouter({
       path: '/dashboard',
       name: 'dashboard',
       component: DashboardView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    },
+    {
+      path: '/faq',
+      name: 'faq',
+      component: FaqView
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: ContactView
     }
+
   ]
 })
 
---
+++ frontend/src/views/aboutView.vue
@@ -0,0 +1,18 @@
+<script>
+import NavBar from '../components/NavBar.vue';
+export default
+	{
+		components: {
+			NavBar,
+		}
+	}
+
+
+</script>
+
+<template >
+	<div class="h-screen bg-slate-300">
+		<NavBar />
+	</div>
+</template>
+
