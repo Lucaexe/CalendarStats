52f1bd4e287d8227ce5d4b680615ab96f3c6ad75
Lucaexe <lucarizzi04.lr@gmail.com>
2024-08-17 11:33:28 +0200

    create first filters form for dashboard

--- frontend/src/views/dashboardView.vue
+++ frontend/src/views/dashboardView.vue
@@ -11,6 +11,11 @@ export default
 		},
 		data: function () {
 			return {
+				eventsList: [
+					{ value: 'vuejs', label: 'Vue.js' },
+					{ value: 'react', label: 'React', disabled: true },
+					{ value: 'angularjs', label: 'AngularJS', disabled: true },
+				],
 				chartOptions: {
 					chart: {
 						id: "vuechart-example",
@@ -32,7 +37,7 @@ export default
 				optionsArea: {
 					labels: ['INNAMORATO', 'Binary System']
 				},
-				seriesArea:[44, 55]
+				seriesArea: [44, 55]
 			};
 		}
 	}
@@ -44,37 +49,53 @@ export default
 	<div class="bg-slate-300">
 		<NavBar />
 		<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-		  <!-- Prima riga su schermi grandi, colonna su schermi piccoli -->
-		  <div class="border-2 border-green-500 m-2">
-			01
-		  </div>
-	  
-		  <div class="border-2 border-green-500 m-2">
-			02
-		  </div>
-	  
-		  <div class="border-2 border-green-500 m-2">
-			03
-		  </div>
-	  
-		  <!-- Seconda riga -->
-		  <div class="border-2 border-green-500 m-2 col-span-1 lg:col-span-2">
-			<apexchart height="400" type="line" :options="chartOptions" :series="series"></apexchart>
-		  </div>
-	  
-		  <div class="border-2 border-green-500 m-2 col-span-1">
-			<apexchart height="400" type="donut" :options="optionsArea" :series="seriesArea"></apexchart>
-		  </div>
-	  
-		  <div class="border-2 border-green-500 m-2">
-			06
-		  </div>
-	  
-		  <div class="border-2 border-green-500 m-2 col-span-1 lg:col-span-2">
-			07
-		  </div>
-		</div>
-	  </div>
-	  
+			<!-- Prima riga su schermi grandi, colonna su schermi piccoli -->
+			<div class="col-span-3 m-2">
+				<Vueform :columns="{ container: 12 }">
+					<MultiselectElement 
+						name="multiselect" 
+						:columns="{ container: 9, label: 12, wrapper: 12 }" 
+						:native="false" 
+						:items="eventsList" 
+					/>
 
+					<ButtonElement 
+						name="register" 
+						:columns="{ container: 3 }" 
+						:submits="true"
+						button-label="Create Dashboard" 
+						:full="true" 
+					/>
+				</Vueform>
+			</div>
+			<div class="border-2 border-green-500 m-2">
+				01
+			</div>
+
+			<div class="border-2 border-green-500 m-2">
+				02
+			</div>
+
+			<div class="border-2 border-green-500 m-2">
+				03
+			</div>
+
+			<!-- Seconda riga -->
+			<div class="border-2 border-green-500 m-2 col-span-1 lg:col-span-2">
+				<apexchart height="400" type="line" :options="chartOptions" :series="series"></apexchart>
+			</div>
+
+			<div class="border-2 border-green-500 m-2 col-span-1">
+				<apexchart height="400" type="donut" :options="optionsArea" :series="seriesArea"></apexchart>
+			</div>
+
+			<div class="border-2 border-green-500 m-2">
+				06
+			</div>
+
+			<div class="border-2 border-green-500 m-2 col-span-1 lg:col-span-2">
+				07
+			</div>
+		</div>
+	</div>
 </template>
